d32f4c5c645aa59450f7e3b26bb48a32e714e163
vinita gupta <vinita.gupta@sapient.com>
2019-04-10 19:18:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+new Vue({
+	el: '#excercise',
+  data: { 
+  	show: true,
+    newArray: 						['pen','glass','scissor','tape'],
+    favBook: {
+    title: 'Lord of the Rings',
+    author: 'J.R.R tolkiens',
+    books: '3'
+    }
+  },
+  	myObject: {
+  		name:'TESTOBJECT',
+      id:10,
+      data: [1.67,1.33,0.98,2.21]
+  	},
+  methods: {
+  }
+	
+})
